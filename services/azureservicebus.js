2b8d23eb0e507c3ba15e61ef4713430f3dced931
Abel Landeo <abel.landeo@gmail.com>
2022-01-23 21:49:11 -0500

    minor improvement on peek

--- services/azureservicebus.js
+++ services/azureservicebus.js
@@ -33,11 +33,13 @@ const azureservicebus = {
                 deadLetterReason: message.deadLetterReason,
                 deadLetterErrorDescription: message.deadLetterErrorDescription,
                 enqueuedSequenceNumber: message.enqueuedSequenceNumber,
-                enqueuedTimeUtc: message.enqueuedTimeUtc
+                enqueuedTimeUtc: message.enqueuedTimeUtc,
+                applicationProperties: message.applicationProperties
             };
             return returnableMessage;            
         });
         return returnableMessages;
+        
     },
 
     receive: async (type, subscription, isDeadLetter, limit) => {
